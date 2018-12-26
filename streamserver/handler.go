a6a6102019782b78ac1530887c6ad5238ab741af
DESKTOP-6UQMCLI\chenxl <chenxinli@gosun.com>
2018-12-26 16:04:13 +0800

    play video from aliyun oss

--- streamserver/handler.go
+++ streamserver/handler.go
@@ -7,21 +7,23 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"time"
 )
 
 func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	vid := p.ByName("vid-id")
-	vl := fmt.Sprintf("%s/%s", VIDEO_DIR, vid)
-	video, err := os.Open(vl)
-	if err != nil {
-		sendErrorResponse(w, http.StatusInternalServerError, "Video open Failed")
-		return
-	}
-	w.Header().Set("Content-Type", "video/mp4")
-	http.ServeContent(w, r, "", time.Now(), video)
-	defer video.Close()
+	//vl := fmt.Sprintf("%s/%s", VIDEO_DIR, vid)
+	//video, err := os.Open(vl)
+	//if err != nil {
+	//	sendErrorResponse(w, http.StatusInternalServerError, "Video open Failed")
+	//	return
+	//}
+	//w.Header().Set("Content-Type", "video/mp4")
+	//http.ServeContent(w, r, "", time.Now(), video)
+	//defer video.Close()
+
+	// paly video from aliyun oss
+	targetUrl := "cyning-video-server.oss-cn-shanghai.aliyuncs.com/videos/" + vid
+	http.Redirect(w, r, targetUrl, 301)
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
